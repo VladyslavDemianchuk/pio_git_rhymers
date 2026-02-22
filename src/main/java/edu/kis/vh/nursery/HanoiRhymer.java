3b6b7934777d1b3493376df846213437bde3024b
Vladyslav Demianchuk <253808@edu.p.lodz.pl>
2026-02-22 16:30:49 +0100

    2. poprawa formatowania w klasie HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -5,13 +5,14 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
     int totalRejected = 0;
 
     public int reportRejected() {
+
         return totalRejected;
     }
 
     public void countIn(int in) {
-        if (!callCheck() && in > peekaboo())
-            totalRejected++;
-        else
-            super.countIn(in);
+
+        if (!callCheck() && in > peekaboo()) totalRejected++;
+
+        else super.countIn(in);
     }
 }
